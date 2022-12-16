fefa78c9dbbbd0633a4bf796de6db653f8fe546a
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-16 20:43:34 -0300

    chore: config of back-end

---
+++ back-end/src/config/config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+const config = {
+username: process.env.MYSQLUSER,
+password: process.env.MYSQLPASSWORD,
+database: process.env.MYSQLDATABASE,
+host: process.env.MYSQLHOST,
+dialect: 'mysql',
+};
+
+module.exports = {
+development: config,
+test: config,
+production: config,
+};
