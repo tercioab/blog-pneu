309896d913d2af25bc32255948b222fa06c221ad
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-18 22:16:49 -0300

    chore: document material ui

---
+++ front-end/src/styles/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/system";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fad390'
+        }
+    }
+})
+
+export default theme
