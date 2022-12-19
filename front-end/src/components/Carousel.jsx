f1d83ecdd1f34adb15a2f25111174d72d38e5c88
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-19 11:46:36 -0300

    style: text align

--- front-end/src/components/Carousel.jsx
+++ front-end/src/components/Carousel.jsx
@@ -7,16 +7,16 @@ export default function CarouselComponent() {
     return (
         <Carousel className={styles.carousel} autoPlay={5000}>
 
-            <div  >
+            <div>
                 <h2 style={{
                     position: 'absolute',
                     color: 'white',
                     position: 'absolute',
                     top: '50%',
-                    left: '50%',
+                    left: '20%',
                     zIndex: 1
                 }}>Titulo da Materia Aqui </h2>
-                <img src="https://images.unsplash.com/photo-1578844251758-2f71da64c96f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="Image 1" />
+                <img src="https://images.unsplash.com/photo-1599474151460-d3439fa608a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80" alt="Image 1" />
                 
                
             </div>
