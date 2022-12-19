214ca5d574fb04cda7a65261304c91ed896782d7
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-19 11:37:14 -0300

    feat: add text on carousel

--- front-end/src/components/Carousel.jsx
+++ front-end/src/components/Carousel.jsx
@@ -5,11 +5,25 @@ import styles from '../styles/Carousel.module.css'
 export default function CarouselComponent() {
      
     return (
-        <Carousel autoPlay={5000}>
+        <Carousel className={styles.carousel} autoPlay={5000}>
+
+            <div  >
+                <h2 style={{
+                    position: 'absolute',
+                    color: 'white',
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    zIndex: 1
+                }}>Titulo da Materia Aqui </h2>
+                <img src="https://images.unsplash.com/photo-1578844251758-2f71da64c96f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80" alt="Image 1" />
+                
+               
+            </div>
         
-            <img className={styles.imagem }src="https://arquivo.devmedia.com.br/marketing/img/guia-css-38149.png" alt="Image 1" />
-            <img className={styles.imagem }src="https://arquivo.devmedia.com.br/marketing/img/guia-css-38149.png" alt="Image 1" />
-            <img className={styles.imagem }src="https://arquivo.devmedia.com.br/marketing/img/guia-css-38149.png" alt="Image 1" />
+        
+            
+        
         </Carousel>
     )
 }
--- front-end/src/pages/_app.jsx
+++ front-end/src/pages/_app.jsx
@@ -4,10 +4,11 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Head from 'next/head';
 import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
+// import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../styles/theme';
 import createEmotionCache from '../utils/createEmotionCache';
+import '../styles/globals.css'
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -20,7 +21,7 @@ export default function MyApp(props) {
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-   <CssBaseline />
+ 
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
