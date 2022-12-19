84c48bebb3199150922ff5ff5dfb9381ce036bc5
walthercio Almeida <suaagenciainicial@gmail.com>
2022-12-18 21:45:03 -0300

    change navbar

--- front-end/src/components/Header.jsx
+++ front-end/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -11,23 +11,17 @@ import MenuItem from '@mui/material/MenuItem';
 import MenuIcon from '@mui/icons-material/Menu';
 
 
-const pages = ['Custo Do Km','Inicio'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+const pages = [{name: "Custo Do Km", link: "custokm"}, {name: "inicio", link: ""}];
 
 function Header() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
+  const handleOpenNavMenu = ({currentTarget}) => {
+    setAnchorElNav(currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
   };
 
   return (
@@ -82,13 +76,21 @@ function Header() {
                 display: { xs: 'block', md: 'none' },
               }}
             >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
+              {pages.map(({link, name}) => (
+                <MenuItem key={name}  onClick={handleCloseNavMenu}>
+           <Button
+                key={name}
+                onClick={handleCloseNavMenu}
+                          sx={{ my: 0, color: 'black' }}
+                          href={`/${link}`} 
+              >
+                {name}
+              </Button>
                 </MenuItem>
               ))}
             </Menu>
-          </Box>
+            </Box>
+                  
           <Typography
             variant="h5"
             noWrap
@@ -106,43 +108,21 @@ function Header() {
             }}
           >
             CUSTO POR KM
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
+            </Typography>
+                  
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map(({link, name}) => (
               <Button
-                key={page}
+                key={name}
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'white', display: 'block' }}
+                href={`/${link}`} 
               >
-                {page}
+                {name}
               </Button>
             ))}
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+            </Box>
+                  
         </Toolbar>
       </Container>
     </AppBar>
